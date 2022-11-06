96690ca22b1123801ba66c85c4ad800c86263d23
giangduong1209 <giangduong120900@gmail.com>
2022-11-06 21:06:11 +0700

    style: change position gg translate

--- src/components/MainFooter/MainFooter.jsx
+++ src/components/MainFooter/MainFooter.jsx
@@ -232,7 +232,12 @@ const MainFooter = () => {
           </Row>
         </div>
       )}
-
+      <div
+        style={{
+          background: "linear-gradient(180deg, #0e0a35 0%, #0e0a35 100%)",
+        }}
+        id="google_translate_element"
+      ></div>
       <div className={styles.container}>
         <Row className={styles.bottomFooter} justify="space-between">
           <Col>
@@ -275,9 +280,7 @@ const MainFooter = () => {
               </div> */}
           {/* </div> */}
           {/* </Col> */}
-          <Col>
-            <div id="google_translate_element"></div>
-          </Col>
+          <Col></Col>
         </Row>
       </div>
     </Footer>
