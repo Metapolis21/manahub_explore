5a7512645317143d8db6cb057d0ef0d2401a5271
giangduong1209 <giangduong120900@gmail.com>
2022-12-06 17:21:09 +0700

    fix: img my collection

--- src/hooks/useIPFS.js
+++ src/hooks/useIPFS.js
@@ -3,10 +3,11 @@ import Constants from "constant";
 export const useIPFS = () => {
   const resolveLink = (url) => {
     if (!url || !url.includes("ipfs://")) return url;
-    return url.replace(
-      "ipfs://",
-      `https://${Constants.GATEWAY_HOSTNAME}/ipfs/`
-    );
+    let linkImage = url.replace("ipfs://", "");
+    let arrStr = linkImage.split("/");
+    let nameImage = arrStr[1];
+    const imageUrl = Constants.GOOGLE_CLOUD_STORAGE_BUCKET + nameImage;
+    return imageUrl;
   };
 
   return { resolveLink };
